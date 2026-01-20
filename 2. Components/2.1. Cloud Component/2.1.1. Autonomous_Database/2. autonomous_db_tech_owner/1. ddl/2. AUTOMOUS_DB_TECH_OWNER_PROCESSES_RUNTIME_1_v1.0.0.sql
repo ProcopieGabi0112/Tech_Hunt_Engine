13071df39fd59ee902888023811eabd14c853585
Procopie Gabriel <36031339+ProcopieGabi0112@users.noreply.github.com>
2026-01-21 01:05:29 +0200

    ds

--- 2. Components/2.1. Cloud Component/2.1.1. Autonomous_Database/2. autonomous_db_tech_owner/1. ddl/2. AUTOMOUS_DB_TECH_OWNER_PROCESSES_RUNTIME_1_v1.0.0.sql
+++ 2. Components/2.1. Cloud Component/2.1.1. Autonomous_Database/2. autonomous_db_tech_owner/1. ddl/2. AUTOMOUS_DB_TECH_OWNER_PROCESSES_RUNTIME_1_v1.0.0.sql
@@ -1,5 +1,5 @@
---AUTONOMOUS_DB_TECH_OWNER_PROCESSES_RUNTIME_1_v1.0.0
---"PROCESSES RUNTIME TABLE"
+--AUTONOMOUS_DB_TECH_OWNER_PROCESSES_NOTIF_1_v1.0.0
+--"PROCESSES NOTIF TABLE"
 SET SERVEROUTPUT ON;
 DECLARE
   v_count NUMBER;
@@ -23,41 +23,32 @@ IF v_count = 0 THEN
 -- Container_Name: "G90CE4847B77DFA_TECHHUNTENGINEDB"
 -- Database_Type: "Pluggable Database (PDB)"
 
---DELETE TABLE PROCESSES_RUNTIME IF EXIST;
+--DELETE TABLE PROCESSES_NOTIF IF EXIST;
 SELECT COUNT(*) INTO v_count
 FROM all_tables
 WHERE owner = 'AUTONOMOUS_DB_TECH_OWNER'
-AND table_name = 'PROCESSES_RUNTIME'
+AND table_name = 'PROCESSES_NOTIF'
 AND tablespace_name = 'DATA';
 IF v_count > 0 THEN
-    EXECUTE IMMEDIATE 'DROP TABLE autonomous_db_tech_owner.processes_runtime CASCADE CONSTRAINTS';
+    EXECUTE IMMEDIATE 'DROP TABLE autonomous_db_tech_owner.processes_notif CASCADE CONSTRAINTS';
 END IF;
---CREATE PROCESSES_RUNTIME TABLE;
+--CREATE PROCESSES_NOTIF TABLE;
 v_sql := q'[
-        CREATE TABLE autonomous_db_tech_owner.processes_runtime (
-          process_code NUMBER(38,0) PRIMARY KEY,
-          process_name VARCHAR2(200) NOT NULL,
-          process_type VARCHAR2(30) NOT NULL CHECK (process_type IN ('GROUP','PROCESS','JOB')),
-          parent_process NUMBER(38,0),
-          execution_mode VARCHAR2(20) DEFAULT 'SEQUENTIAL' NOT NULL CHECK (execution_mode IN ('PARALLEL','SEQUENTIAL')),
-          run_order NUMBER(38,0) NOT NULL,
-          start_time VARCHAR2(50),
-          start_condition VARCHAR2(2000),
-          expected_rows NUMBER(5,0),
-          waiting_time NUMBER(15,0),
-          retry_time NUMBER(15,0),
-          is_active  VARCHAR2(1) DEFAULT 'Y' NOT NULL CHECK (is_active IN ('Y','N')),
-          job_name  VARCHAR2(200),
-          chain_name  VARCHAR2(200),
-          program_name VARCHAR2(200),
-          description VARCHAR2(500),
+        CREATE TABLE autonomous_db_tech_owner.processes_notif (
+
+          notification_id      VARCHAR2(200) PRIMARY KEY,
+          process_name         VARCHAR2(100) NOT NULL,
+          process_date         VARCHAR2(10) NOT NULL,
+          process_type         VARCHAR2(30) NOT NULL,
+          start_timestamp      TIMESTAMP NOT NULL,
+          end_timestamp        TIMESTAMP NOT NULL,
+          status               VARCHAR2(30) NOT NULL CHECK (status IN ('IN_PROGRESS','ERROR','DONE')),
+          error_message        VARCHAR2(250),
+          admin_user           VARCHAR2(50) NOT NULL,
 
           creation_date         TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
           created_by            VARCHAR2(50) NOT NULL, 
-          last_update_date      TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-          last_updated_by       VARCHAR2(50) NOT NULL, 
-          source_system         VARCHAR2(20) DEFAULT 'db_env' NOT NULL CHECK (source_system IN ('db_env','dw_env','pg_env')),
-          CONSTRAINT fk_parent_process FOREIGN KEY (parent_process) REFERENCES processes_runtime(process_code)
+          source_system         VARCHAR2(20) DEFAULT 'db_env' NOT NULL CHECK (source_system IN ('db_env','dw_env','pg_env'))
         )
     ]';
 EXECUTE IMMEDIATE v_sql;
@@ -65,72 +56,115 @@ EXECUTE IMMEDIATE v_sql;
 SELECT COUNT(*) INTO v_count
 FROM all_tables
 WHERE owner = 'AUTONOMOUS_DB_TECH_OWNER'
-AND table_name = 'PROCESSES_RUNTIME'
+AND table_name = 'PROCESSES_NOTIF'
 AND tablespace_name = 'DATA';
 IF v_count = 0 THEN
-    RAISE_APPLICATION_ERROR(-20001,'The PROCESSES_RUNTIME table wasnt created properly.');
+    RAISE_APPLICATION_ERROR(-20001,'The PROCESSES_NOTIF table wasnt created properly.');
 END IF;
-DBMS_OUTPUT.PUT_LINE('[3.] The PROCESSES_RUNTIME table was created.');
+DBMS_OUTPUT.PUT_LINE('[3.] The PROCESSES_NOTIF table was created.');
 --CREATE COMMENTS FROM TABLE AND COLUMNS
 -- TABLE COMMENT
-EXECUTE IMMEDIATE 'COMMENT ON TABLE autonomous_db_tech_owner.processes_runtime IS ''The table contains the information about the processes that will move data through database.''';
+EXECUTE IMMEDIATE 'COMMENT ON TABLE autonomous_db_tech_owner.processes_notif IS ''The table contains the informations about the notifications that will be display when a process will start.''';
 -- BUSINESS COLUMNS COMMENT
-EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_runtime.process_code IS ''The code of the process.''';
-EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_runtime.process_name IS ''The name of the process.''';
-EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_runtime.process_type IS ''The type of the process.''';
-EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_runtime.parent_process IS ''The code of the parent process.''';
-EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_runtime.execution_mode IS ''The execution mode of the process.''';
-EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_runtime.run_order IS ''The run order of the process.''';
-EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_runtime.start_time IS ''The start time of the process.''';
-EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_runtime.start_condition IS ''The start condition of the process.''';
-EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_runtime.expected_rows IS ''The expected rows from the start condition of the process.''';
-EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_runtime.waiting_time IS ''The number of seconds that the process wait to start condition to complete.''';
-EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_runtime.retry_time IS ''The number of the seconds that the process retry to see if the start condition was completed.''';
-EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_runtime.is_active IS ''The flag that indicating if the process is active or not.''';
-EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_runtime.job_name IS ''The job name of the process.''';
-EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_runtime.chain_name IS ''The chain name of the process.''';
-EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_runtime.program_name IS ''The program name of the process.''';
-EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_runtime.description IS ''The description of the process.''';
+EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_notif.notification_id IS ''The id of the process.''';
+EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_notif.process_name IS ''The name of the process.''';
+EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_notif.process_date IS ''The date when the process was started.''';
+EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_notif.process_type IS ''The type of the process.''';
+EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_notif.start_timestamp IS ''The start time of the process.''';
+EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_notif.end_timestamp IS ''The end time of the process.''';
+EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_notif.status IS ''The status of the process.''';
+EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_notif.error_message IS ''The error message of the process.''';
+EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_notif.admin_user IS ''The user who start the process.''';
 -- TECHNICAL COLUMNS COMMENT
-EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_runtime.creation_date IS ''Technical Column - The creation date of the record''';
-EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_runtime.created_by IS ''Technical Column - The user who created the record''';
-EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_runtime.last_update_date IS ''Technical Column - The last update date of the record''';
-EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_runtime.last_updated_by IS ''Technical Column - The user who updated the record''';
-EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_runtime.source_system IS ''Technical Column - The source system of the record''';
+EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_notif.creation_date IS ''Technical Column - The creation date of the record''';
+EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_notif.created_by IS ''Technical Column - The user who created the record''';
+EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_tech_owner.processes_notif.source_system IS ''Technical Column - The source system of the record''';
+
+--CREATE SEQUENCE SEQ_PROCESSES_NOTIF_PK FOR PRIMARY KEY
+--DELETE SEQUENCE IF EXISTS;
+SELECT COUNT(*) INTO v_count
+FROM user_sequences
+WHERE sequence_name = 'SEQ_PROCESSES_NOTIF_PK';
+IF v_count > 0 THEN
+    EXECUTE IMMEDIATE 'DROP SEQUENCE autonomous_db_tech_owner.seq_processes_notif_pk';
+END IF;
+--CREATE SEQUENCE
+ EXECUTE IMMEDIATE '
+    CREATE SEQUENCE seq_processes_notif_pk
+    START WITH 1
+    INCREMENT BY 1
+    NOCACHE
+    NOCYCLE
+  ';
+SELECT COUNT(*) INTO v_count
+FROM user_sequences
+WHERE sequence_name = 'SEQ_PROCESSES_NOTIF_PK';
+IF v_count = 0 THEN
+    RAISE_APPLICATION_ERROR(-20001,'The SEQ_PROCESSES_NOTIF_PK sequence wasnt created properly.');
+END IF;
+DBMS_OUTPUT.PUT_LINE('[4.] The SEQ_PROCESSES_NOTIF_PK sequence for primary key was created.');
+
+--CREATE TRIGGER FOR PRIMARY KEY;
+--DELETE TRIGGER IF EXISTS;
+SELECT COUNT(*) INTO v_count
+FROM user_triggers
+WHERE trigger_name = 'trg_processes_notif_pk';
+IF v_count > 0 THEN
+    EXECUTE IMMEDIATE 'DROP TRIGGER autonomous_db_tech_owner.trg_processes_notif_pk';
+END IF;
+--CREATE TRIGGER
+v_sql := q'[
+CREATE OR REPLACE TRIGGER trg_processes_notif_pk
+BEFORE INSERT ON autonomous_db_tech_owner.processes_notif
+FOR EACH ROW
+BEGIN
+   IF :NEW.notification_id IS NULL OR TRIM(:NEW.notification_id) = '' THEN
+      SELECT TO_CHAR(seq_processes_notif_pk.NEXTVAL)
+      INTO :NEW.notification_id
+      FROM dual;
+   END IF;
+END;
+]';
+      
+EXECUTE IMMEDIATE v_sql;
+--CHECK IF THE TRiGGER WAS CREATED;
+SELECT COUNT(*) INTO v_count
+FROM user_triggers
+WHERE trigger_name = 'TRG_PROCESSES_NOTIF_PK';
+IF v_count = 0 THEN
+    RAISE_APPLICATION_ERROR(-20001,'The TRG_PROCESSES_NOTIF_PK trigger wasnt created properly.');
+END IF;
+DBMS_OUTPUT.PUT_LINE('[5.] The TRG_PROCESSES_NOTIF_PK trigger for primary key was created.');
 
 --CREATE TRIGGER FOR TECHNICAL COLUMNS
 --DELETE TRIGGER IF EXISTS;
 SELECT COUNT(*) INTO v_count
 FROM user_triggers
-WHERE trigger_name = 'TRG_PROCESSES_RUNTIME_TECH_COL';
+WHERE trigger_name = 'TRG_PROCESSES_NOTIF_TECH_COL';
 IF v_count > 0 THEN
-    EXECUTE IMMEDIATE 'DROP TRIGGER autonomous_db_tech_owner.trg_processes_runtime_tech_col';
+    EXECUTE IMMEDIATE 'DROP TRIGGER autonomous_db_tech_owner.trg_processes_notif_tech_col';
 END IF;
 --CREATE TRIGGER
-v_sql := '  CREATE OR REPLACE TRIGGER trg_processes_runtime_tech_col
-            BEFORE INSERT ON autonomous_db_tech_owner.processes_runtime
+v_sql := '  CREATE OR REPLACE TRIGGER trg_processes_notif_tech_col
+            BEFORE INSERT ON autonomous_db_tech_owner.processes_notif
             FOR EACH ROW
             BEGIN
                  IF INSERTING THEN
                     :NEW.created_by := USER;
                     :NEW.creation_date := CURRENT_TIMESTAMP;
                  END IF;
-                 IF UPDATING THEN
-                    :NEW.last_updated_by := USER;
-                    :NEW.last_update_date := CURRENT_TIMESTAMP;
-                 END IF;
             END;';
 EXECUTE IMMEDIATE v_sql;
 --CHECK IF THE TRiGGER WAS CREATED;
 SELECT COUNT(*) INTO v_count
 FROM user_triggers
-WHERE trigger_name = 'TRG_PROCESSES_RUNTIME_TECH_COL';
+WHERE trigger_name = 'TRG_PROCESSES_NOTIF_TECH_COL';
 IF v_count = 0 THEN
-    RAISE_APPLICATION_ERROR(-20001,'The TRG_PROCESSES_RUNTIME_TECH_COL trigger wasnt created properly.');
+    RAISE_APPLICATION_ERROR(-20001,'The TRG_PROCESSES_NOTIF_TECH_COL trigger wasnt created properly.');
 END IF;
-DBMS_OUTPUT.PUT_LINE('[4.] The TRG_PROCESSES_RUNTIME_TECH_COL trigger for technical columns was created.');
+DBMS_OUTPUT.PUT_LINE('[6.] The TRG_PROCESSES_NOTIF_TECH_COL trigger for technical columns was created.');
 
-DBMS_OUTPUT.PUT_LINE('[5.] The script running is done!');
+DBMS_OUTPUT.PUT_LINE('[7.] The script running is done!');
 EXCEPTION
   WHEN OTHERS THEN
     DBMS_OUTPUT.PUT_LINE('ERROR: ' || SQLERRM);
