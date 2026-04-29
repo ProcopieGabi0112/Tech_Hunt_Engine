c25ddb975516401cdce5d104e69da3c1e81460a2
TGrc30 <dprocopie@yahoo.com>
2026-04-29 06:16:23 +0300

    d
    d

---
+++ 2. Components/2.1. Cloud Component/2.1.1. Autonomous_Database/4. autonomous_db_out_owner  (wip)/1. ddl/1. skill_area [13-17,5]/15_1. AUTONOMOUS_DB_OUT_OWNER_VERSION_V_VW_1_v1.0.0.sql
@@ -0,0 +1,80 @@
+--AUTONOMOUS_DB_OUT_OWNER_VERSION_V_VW_1_v1.0.0
+--"TECHNOLOGY_V VIEW"
+SET SERVEROUTPUT ON;
+DECLARE
+  v_count NUMBER;
+  v_text  VARCHAR2(4000);
+  v_sql CLOB;
+BEGIN
+DBMS_OUTPUT.PUT_LINE('[1.] Script running...');
+
+--[1.] CHECK CURRENT USER AND SCHEMA
+SELECT COUNT(*) INTO v_count
+FROM dual
+WHERE sys_context('USERENV', 'SESSION_USER') = 'AUTONOMOUS_DB_OUT_OWNER'
+AND sys_context('USERENV', 'CON_NAME') = 'G90CE4847B77DFA_TECHHUNTENGINEDB' 
+AND sys_context('USERENV', 'DB_NAME') = 'G90CE4847B77DFA_TECHHUNTENGINEDB';
+IF v_count = 0 THEN
+    RAISE_APPLICATION_ERROR(-20001,'The environment is wrong. Please check the user.');
+  END IF;
+  DBMS_OUTPUT.PUT_LINE('[2.] The environment is preparing...');
+-- Current_User: "AUTONOMOUS_DB_OWNER"	
+-- Database_name: "G90CE4847B77DFA_TECHHUNTENGINEDB"	
+-- Container_Name: "G90CE4847B77DFA_TECHHUNTENGINEDB"
+-- Database_Type: "Pluggable Database (PDB)"
+
+--DELETE TABLE technology_type IF EXIST;
+SELECT COUNT(*) INTO v_count
+FROM all_views
+WHERE owner = 'AUTONOMOUS_DB_OUT_OWNER'
+AND view_name = 'VERSION_V';
+IF v_count > 0 THEN
+    EXECUTE IMMEDIATE 'DROP VIEW autonomous_db_out_owner.version_v CASCADE CONSTRAINTS';
+END IF;
+--CREATE TECHNOLOGY_TYPE TABLE;
+v_sql := q'[
+CREATE OR REPLACE VIEW autonomous_db_out_owner.version_v AS
+SELECT 
+version_code,
+name,
+release_date,
+end_of_life,
+new_features,
+unsolved_problems,
+creator,
+developer_popularity,
+community_support,
+industry_usage_score,
+knowledge_score,
+skills_rating,
+rating,
+description,
+technology_code,
+creation_date,
+created_by,
+last_update_date,
+last_updated_by,
+source_system,
+sync_status,
+sync_version,
+last_synced_at,
+deleted_flag
+FROM autonomous_db_owner.version;
+]';
+EXECUTE IMMEDIATE v_sql;
+--[1.] VERIFY IF THE TABLE WAS CREATED RIGHT
+SELECT COUNT(*) INTO v_count
+FROM all_views
+WHERE owner = 'AUTONOMOUS_DB_OUT_OWNER'
+AND view_name = 'VERSION_V';
+IF v_count = 0 THEN
+    RAISE_APPLICATION_ERROR(-20001,'The VERSION_V view wasnt created properly.');
+END IF;
+DBMS_OUTPUT.PUT_LINE('[2.] The VERSION_V view was created.');
+
+DBMS_OUTPUT.PUT_LINE('[6.] The script running is done!');
+EXCEPTION
+  WHEN OTHERS THEN
+    DBMS_OUTPUT.PUT_LINE('ERROR: ' || SQLERRM);
+END;
+/
