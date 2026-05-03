de2d220da175b8f48161411f9432d5e6e0123a4d
Procopie Gabriel <36031339+ProcopieGabi0112@users.noreply.github.com>
2026-05-03 18:33:08 +0300

    d
    d

--- 2. Components/2.1. Cloud Component/2.1.1. Autonomous_Database/4. autonomous_db_out_owner  (wip)/1. ddl/3. user_area [18-20,29-35,10]/35_1. AUTONOMOUS_DB_OUT_OWNER_USER_LEVEL_V_VW_1_v1.0.0.sql
+++ 2. Components/2.1. Cloud Component/2.1.1. Autonomous_Database/4. autonomous_db_out_owner  (wip)/1. ddl/3. user_area [18-20,29-35,10]/35_1. AUTONOMOUS_DB_OUT_OWNER_USER_LEVEL_V_VW_1_v1.0.0.sql
@@ -1,5 +1,5 @@
---AUTONOMOUS_DB_OUT_OWNER_UTILIZATORI_V_VW_1_v1.0.0
---"UTILIZATORI_V VIEW"
+--AUTONOMOUS_DB_OUT_OWNER_USER_LEVEL_V_VW_1_v1.0.0
+--"USER_LEVEL_V VIEW"
 SET SERVEROUTPUT ON;
 DECLARE
   v_count NUMBER;
@@ -23,38 +23,21 @@ IF v_count = 0 THEN
 -- Container_Name: "G90CE4847B77DFA_TECHHUNTENGINEDB"
 -- Database_Type: "Pluggable Database (PDB)"
 
---DELETE TABLE utilizatori_v IF EXIST;
+--DELETE TABLE user_level IF EXIST;
 SELECT COUNT(*) INTO v_count
 FROM all_views
 WHERE owner = 'AUTONOMOUS_DB_OUT_OWNER'
-AND view_name = 'UTILIZATORI_V';
+AND view_name = 'USER_LEVEL_V';
 IF v_count > 0 THEN
-    EXECUTE IMMEDIATE 'DROP VIEW autonomous_db_out_owner.utilizatori_v CASCADE CONSTRAINTS';
+    EXECUTE IMMEDIATE 'DROP VIEW autonomous_db_out_owner.user_level_v CASCADE CONSTRAINTS';
 END IF;
---CREATE utilizatori_v TABLE;
+--CREATE USER_LEVEL TABLE;
 v_sql := q'[
-CREATE OR REPLACE VIEW autonomous_db_out_owner.utilizatori_v AS
+CREATE OR REPLACE VIEW autonomous_db_out_owner.user_level_v AS
 SELECT 
-user_id,              
-email,               
-first_name,          
-last_name,           
-user_name,            
-app_email,             
-password,	            
-date_of_birth,	       
-phone,	              
-gender,	            
-profile_image,        
-profile_document,     
-account_status,	    
-profile_approved_flag, 
-report_sent_flag,	    
-report_document,      
-native_lang_code,	  
-supervizor_id,	       
-location_id,           
-role_id,           
+user_id,
+lang_level_id,
+obtained_date, 
 creation_date,
 created_by,
 last_update_date,
@@ -64,18 +47,18 @@ sync_status,
 sync_version,
 last_synced_at,
 deleted_flag
-FROM autonomous_db_owner.utilizatori;
+FROM autonomous_db_owner.user_level;
 ]';
 EXECUTE IMMEDIATE v_sql;
 --[1.] VERIFY IF THE TABLE WAS CREATED RIGHT
 SELECT COUNT(*) INTO v_count
 FROM all_views
 WHERE owner = 'AUTONOMOUS_DB_OUT_OWNER'
-AND view_name = 'UTILIZATORI_V';
+AND view_name = 'USER_LEVEL_V';
 IF v_count = 0 THEN
-    RAISE_APPLICATION_ERROR(-20001,'The UTILIZATORI_V view wasnt created properly.');
+    RAISE_APPLICATION_ERROR(-20001,'The USER_LEVEL_V view wasnt created properly.');
 END IF;
-DBMS_OUTPUT.PUT_LINE('[3.] The UTILIZATORI_V view was created.');
+DBMS_OUTPUT.PUT_LINE('[3.] The USER_LEVEL_V view was created.');
 
 DBMS_OUTPUT.PUT_LINE('[4.] The script running is done!');
 EXCEPTION
