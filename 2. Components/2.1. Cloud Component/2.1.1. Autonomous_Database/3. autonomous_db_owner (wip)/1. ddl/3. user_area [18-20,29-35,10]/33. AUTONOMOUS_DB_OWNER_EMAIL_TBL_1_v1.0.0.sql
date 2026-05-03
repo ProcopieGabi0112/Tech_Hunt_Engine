cee0f86dbb728ceaaf30924343cdb4105472eef8
TGrc30 <dprocopie@yahoo.com>
2026-05-03 21:40:32 +0300

    dd
    dd

---
+++ 2. Components/2.1. Cloud Component/2.1.1. Autonomous_Database/3. autonomous_db_owner (wip)/1. ddl/3. user_area [18-20,29-35,10]/33. AUTONOMOUS_DB_OWNER_EMAIL_TBL_1_v1.0.0.sql
@@ -0,0 +1,149 @@
+--AUTONOMOUS_DB_OWNER_EMAIL_TBL_1_v1.0.0
+--"EMAIL TABLE"
+SET SERVEROUTPUT ON;
+DECLARE
+  v_count NUMBER;
+  v_text  VARCHAR2(4000);
+  v_sql CLOB;
+BEGIN
+DBMS_OUTPUT.PUT_LINE('[1.] Script running...');
+
+--[1.] CHECK CURRENT USER AND SCHEMA
+SELECT COUNT(*) INTO v_count
+FROM dual
+WHERE sys_context('USERENV', 'SESSION_USER') = 'AUTONOMOUS_DB_OWNER'
+AND sys_context('USERENV', 'CON_NAME') = 'G90CE4847B77DFA_TECHHUNTENGINEDB' 
+AND sys_context('USERENV', 'DB_NAME') = 'G90CE4847B77DFA_TECHHUNTENGINEDB';
+IF v_count = 0 THEN
+    RAISE_APPLICATION_ERROR(-20001,'The environment is wrong. Please check the user.');
+  END IF;
+  DBMS_OUTPUT.PUT_LINE('[2.] The environment is preparing...');
+-- Current_User: "AUTONOMOUS_DB_OWNER"	
+-- Database_name: "G90CE4847B77DFA_TECHHUNTENGINEDB"	
+-- Container_Name: "G90CE4847B77DFA_TECHHUNTENGINEDB"
+-- Database_Type: "Pluggable Database (PDB)"
+
+--DELETE TABLE email IF EXIST;
+SELECT COUNT(*) INTO v_count
+FROM all_tables
+WHERE owner = 'AUTONOMOUS_DB_OWNER'
+AND table_name = 'EMAIL'
+AND tablespace_name = 'DATA';
+IF v_count > 0 THEN
+    EXECUTE IMMEDIATE 'DROP TABLE autonomous_db_owner.email CASCADE CONSTRAINTS';
+END IF;
+--CREATE EMAIL TABLE;
+v_sql := q'[
+        CREATE TABLE autonomous_db_owner.email (
+          --business columns
+          email_code      NUMBER(38,0) PRIMARY KEY,
+          subject         VARCHAR2(250) NOT NULL,
+	      content         VARCHAR2(250),
+          arrival_time    TIMESTAMP, 
+          importance      VARCHAR2(50),
+          reply_to_email  NUMBER(38,0),
+          receiver        NUMBER(38,0),
+          sender          NUMBER(38,0),  
+
+          --technical columns
+          creation_date         TIMESTAMP NOT NULL,
+          created_by            VARCHAR2(50) NOT NULL,
+          last_update_date      TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+          last_updated_by       VARCHAR2(50) NOT NULL,
+          source_system         VARCHAR2(20) DEFAULT 'db_env' NOT NULL CHECK (source_system IN ('db_env','dw_env','pg_env')),
+          sync_status           VARCHAR2(20) DEFAULT 'synced' NOT NULL CHECK (sync_status IN ('synced','not_synced')),
+          sync_version          NUMBER(38,0) DEFAULT 1 NOT NULL,
+          last_synced_at        TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+          deleted_flag          VARCHAR2(1) DEFAULT 'N' NOT NULL CHECK (deleted_flag IN ('N','Y')),
+          
+         CONSTRAINT fk_reply_to_email FOREIGN KEY (reply_to_email) REFERENCES email(email_code),
+         CONSTRAINT fk_receiver FOREIGN KEY (receiver) REFERENCES utilizatori(user_id),
+         CONSTRAINT fk_sender FOREIGN KEY (sender) REFERENCES utilizatori(user_id)
+)
+    ]';
+ 
+EXECUTE IMMEDIATE v_sql;
+EXECUTE IMMEDIATE 'GRANT SELECT ON autonomous_db_owner.email TO autonomous_db_out_owner';
+--[1.] VERIFY IF THE TABLE WAS CREATED RIGHT
+SELECT COUNT(*) INTO v_count
+FROM all_tables
+WHERE owner = 'AUTONOMOUS_DB_OWNER'
+AND table_name = 'EMAIL'
+AND tablespace_name = 'DATA';
+IF v_count = 0 THEN
+    RAISE_APPLICATION_ERROR(-20001,'The EMAIL table wasnt created properly.');
+END IF;
+DBMS_OUTPUT.PUT_LINE('[3.] The EMAIL table was created.');
+--CREATE COMMENTS FROM TABLE AND COLUMNS
+-- TABLE COMMENT
+EXECUTE IMMEDIATE 'COMMENT ON TABLE  autonomous_db_owner.email IS ''The table contains the emails from all users to other users in the application''';
+-- BUSINESS COLUMNS COMMENT                              
+EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_owner.email.email_code IS ''The primary key from email table''';
+EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_owner.email.subject IS ''The subject of the email''';
+EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_owner.email.content IS ''The content of the email''';
+EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_owner.email.arrival_time IS ''The arrival time of the email''';
+EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_owner.email.importance IS ''The importance of the email''';
+EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_owner.email.reply_to_email IS ''The code of the referenced email''';
+EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_owner.email.receiver IS ''The receiver of the email''';
+EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_owner.email.sender IS ''The sender of the email''';
+
+-- TECHNICAL COLUMNS COMMENT                             
+EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_owner.email.creation_date IS ''Technical Column - The creation date of the record''';
+EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_owner.email.created_by IS ''Technical Column - The user who created the record''';
+EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_owner.email.last_update_date IS ''Technical Column - The last update date of the record''';
+EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_owner.email.last_updated_by IS ''Technical Column - The user who updated the record''';
+EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_owner.email.source_system IS ''Technical Column - The source system of the record''';
+EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_owner.email.sync_status IS ''Technical Column - The sync status of the record''';
+EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_owner.email.sync_version IS ''Technical Column - The sync version of the record''';
+EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_owner.email.last_synced_at IS ''Technical Column - The date when the record was last time synced''';
+EXECUTE IMMEDIATE 'COMMENT ON COLUMN autonomous_db_owner.email.deleted_flag IS ''Technical Column - The flag indicating if the record is deleted or not''';
+
+--CREATE TRIGGER FOR TECHNICAL COLUMNS
+--DELETE TRIGGER IF EXISTS;
+SELECT COUNT(*) INTO v_count
+FROM user_triggers
+WHERE trigger_name = 'TRG_EMAIL_TECH_COL';
+IF v_count > 0 THEN
+    EXECUTE IMMEDIATE 'DROP TRIGGER autonomous_db_owner.trg_email_tech_col';
+END IF;
+--CREATE TRIGGER
+v_sql := '  CREATE OR REPLACE TRIGGER trg_email_tech_col
+            BEFORE INSERT OR UPDATE ON autonomous_db_owner.email
+            FOR EACH ROW
+            BEGIN
+                 IF INSERTING THEN
+                    :NEW.created_by := USER;
+                    :NEW.last_updated_by := USER;
+
+                    -- VARIANTA CURENTA
+                    -- dacă utilizatorul NU trimite creation_date, îl setăm noi
+                        IF :NEW.creation_date IS NULL THEN
+                              :NEW.creation_date := CURRENT_TIMESTAMP;
+                        END IF;
+
+                    -- last_update_date = creation_date (mereu la insert)
+                    :NEW.last_update_date := :NEW.creation_date;
+                   
+                   END IF;
+
+                 IF UPDATING THEN
+                    :NEW.last_update_date := CURRENT_TIMESTAMP;
+                    :NEW.last_updated_by := USER;
+                 END IF;
+            END;';
+EXECUTE IMMEDIATE v_sql;
+--CHECK IF THE TRIGGER WAS CREATED;
+SELECT COUNT(*) INTO v_count
+FROM user_triggers
+WHERE trigger_name = 'TRG_EMAIL_TECH_COL';
+IF v_count = 0 THEN
+    RAISE_APPLICATION_ERROR(-20001,'The TRG_EMAIL_TECH_COL trigger wasnt created properly.');
+END IF;
+DBMS_OUTPUT.PUT_LINE('[4.] The TRG_EMAIL_TECH_COL trigger for technical columns was created.');
+
+DBMS_OUTPUT.PUT_LINE('[5.] The script running is done!');
+EXCEPTION
+  WHEN OTHERS THEN
+    DBMS_OUTPUT.PUT_LINE('ERROR: ' || SQLERRM);
+END;
+/
---
+++ 2. Components/2.1. Cloud Component/2.1.1. Autonomous_Database/4. autonomous_db_out_owner  (wip)/1. ddl/3. user_area [18-20,29-35,10]/33_1. AUTONOMOUS_DB_OUT_OWNER_EMAIL_V_VW_1_v1.0.0.sql
@@ -0,0 +1,73 @@
+--AUTONOMOUS_DB_OUT_OWNER_EMAIL_V_VW_1_v1.0.0
+--"EMAIL_V VIEW"
+SET SERVEROUTPUT ON;
+DECLARE
+  v_count NUMBER;
+  v_text  VARCHAR2(4000);
+  v_sql CLOB;
+BEGIN
+DBMS_OUTPUT.PUT_LINE('[1.] Script running...');
+
+--[1.] CHECK CURRENT USER AND SCHEMA
+SELECT COUNT(*) INTO v_count
+FROM dual
+WHERE sys_context('USERENV', 'SESSION_USER') = 'AUTONOMOUS_DB_OUT_OWNER'
+AND sys_context('USERENV', 'CON_NAME') = 'G90CE4847B77DFA_TECHHUNTENGINEDB' 
+AND sys_context('USERENV', 'DB_NAME') = 'G90CE4847B77DFA_TECHHUNTENGINEDB';
+IF v_count = 0 THEN
+    RAISE_APPLICATION_ERROR(-20001,'The environment is wrong. Please check the user.');
+  END IF;
+  DBMS_OUTPUT.PUT_LINE('[2.] The environment is preparing...');
+-- Current_User: "AUTONOMOUS_DB_OWNER"	
+-- Database_name: "G90CE4847B77DFA_TECHHUNTENGINEDB"	
+-- Container_Name: "G90CE4847B77DFA_TECHHUNTENGINEDB"
+-- Database_Type: "Pluggable Database (PDB)"
+
+--DELETE TABLE email_v IF EXIST;
+SELECT COUNT(*) INTO v_count
+FROM all_views
+WHERE owner = 'AUTONOMOUS_DB_OUT_OWNER'
+AND view_name = 'EMAIL_V';
+IF v_count > 0 THEN
+    EXECUTE IMMEDIATE 'DROP VIEW autonomous_db_out_owner.email_v CASCADE CONSTRAINTS';
+END IF;
+--CREATE EMAIL TABLE;
+v_sql := q'[
+CREATE OR REPLACE VIEW autonomous_db_out_owner.email_v AS
+SELECT 
+email_code,
+subject,
+content,
+arrival_time,
+importance,
+reply_to_email,
+receiver,
+sender,
+creation_date,
+created_by,
+last_update_date,
+last_updated_by,
+source_system,
+sync_status,
+sync_version,
+last_synced_at,
+deleted_flag
+FROM autonomous_db_owner.email;
+]';
+EXECUTE IMMEDIATE v_sql;
+--[1.] VERIFY IF THE TABLE WAS CREATED RIGHT
+SELECT COUNT(*) INTO v_count
+FROM all_views
+WHERE owner = 'AUTONOMOUS_DB_OUT_OWNER'
+AND view_name = 'EMAIL_V';
+IF v_count = 0 THEN
+    RAISE_APPLICATION_ERROR(-20001,'The EMAIL_V view wasnt created properly.');
+END IF;
+DBMS_OUTPUT.PUT_LINE('[3.] The EMAIL_V view was created.');
+
+DBMS_OUTPUT.PUT_LINE('[4.] The script running is done!');
+EXCEPTION
+  WHEN OTHERS THEN
+    DBMS_OUTPUT.PUT_LINE('ERROR: ' || SQLERRM);
+END;
+/
